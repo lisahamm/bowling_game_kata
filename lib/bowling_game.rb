12388cb3ced47acdc13a31c2d9ac5a212a82f93a
Lisa Hamm <lisalhamm@gmail.com>
2015-01-08 16:08:26 -0600

    Added code to pass first failing test

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,3 +1,9 @@
 class BowlingGame
+  def roll(pins_down)
+    pins_down
+  end
 
+  def score
+    0
+  end
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -5,7 +5,7 @@ describe BowlingGame do
   let(:bowling_game) {BowlingGame.new}
 
   it "can score a gutter game" do
-    20.times bowling_game.roll(0)
+    20.times {bowling_game.roll(0)}
     expect(bowling_game.score).to eq 0
   end
 end
