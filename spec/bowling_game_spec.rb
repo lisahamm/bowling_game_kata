fe59b0d3ee1b4f11210f8cfffdab756755f20e6b
Lisa Hamm <lisalhamm@gmail.com>
2015-01-08 15:59:07 -0600

    Added first failing test

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -2,5 +2,10 @@ require 'spec_helper'
 require 'bowling_game'
 
 describe BowlingGame do
+  let(:bowling_game) {BowlingGame.new}
 
+  it "can score a gutter game" do
+    20.times bowling_game.roll(0)
+    expect(bowling_game.score).to eq 0
+  end
 end
