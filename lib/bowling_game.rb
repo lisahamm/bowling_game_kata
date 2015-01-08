6920b986769a3853604f6a561ee7b0b738c8314e
Lisa Hamm <lisalhamm@gmail.com>
2015-01-08 16:24:05 -0600

    Added code to score a simple game

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,9 +1,19 @@
 class BowlingGame
+  def initialize
+    @rolls = []
+  end
+
   def roll(pins_down)
-    pins_down
+    @rolls << pins_down
   end
 
   def score
-    0
+    total = 0
+    i = 0
+    while i < 20
+      total += @rolls[i]
+      i += 1
+    end
+    total
   end
 end
