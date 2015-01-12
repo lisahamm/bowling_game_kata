faa0f57c80b78e5cfef07921dba3a992c4eb5049
Lisa Hamm <lisalhamm@gmail.com>
2015-01-12 10:26:14 -0600

    Added code to score a strike

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -11,12 +11,16 @@ class BowlingGame
     total = 0
     i = 0
     while i < @rolls.length-1
-      if @rolls[i] + @rolls[i+1] == 10
+      if @rolls[i] == 10
         total += @rolls[i] + @rolls[i+1] + @rolls [i+2]
+        i += 1
+      elsif @rolls[i] + @rolls[i+1] == 10
+        total += @rolls[i] + @rolls[i+1] + @rolls [i+2]
+        i += 2
       else
         total += @rolls[i] + @rolls[i+1]
+        i += 2
       end
-      i += 2
     end
     total
   end
