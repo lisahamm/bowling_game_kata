a28371d4b490ae65d0fedb099ad31cb6e84afaa1
Lisa Hamm <lisalhamm@gmail.com>
2015-01-12 13:40:21 -0600

    Refactoring

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -11,16 +11,15 @@ class BowlingGame
     total = 0
     frame_index = 0
     frame = 0
-    while frame_index < @rolls.length-1
+    while frame < 10
       if strike?(frame_index)
-        total += 10 + @rolls[frame_index+1] + @rolls [frame_index+2]
-        frame_index += 2 if frame == 9
+        total += 10 + strike_bonus(frame_index)
         frame_index += 1
       elsif spare?(frame_index)
-        total += 10 + @rolls [frame_index+2]
+        total += 10 + spare_bonus(frame_index)
         frame_index += 2
       else
-        total += @rolls[frame_index] + @rolls[frame_index+1]
+        total += sum_of_balls_in_frame(frame_index)
         frame_index += 2
       end
       frame += 1
@@ -34,5 +33,17 @@ class BowlingGame
 
   def spare?(frame_index)
     @rolls[frame_index] + @rolls[frame_index+1] == 10
+  end
+
+  def sum_of_balls_in_frame(frame_index)
+    @rolls[frame_index] + @rolls[frame_index+1]
+  end
+
+  def spare_bonus(frame_index)
+    @rolls[frame_index + 2]
+  end
+
+  def strike_bonus(frame_index)
+    @rolls[frame_index+1] + @rolls[frame_index+2]
   end
 end
