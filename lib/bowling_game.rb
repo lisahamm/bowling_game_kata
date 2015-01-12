1dd9320732677715671d7e8cae157fb2585dcb22
Lisa Hamm <lisalhamm@gmail.com>
2015-01-12 11:50:17 -0600

    Refactored tests

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -9,30 +9,30 @@ class BowlingGame
 
   def score
     total = 0
-    i = 0
+    frame_index = 0
     frame = 0
-    while i < @rolls.length-1
-      if strike?(i)
-        total += @rolls[i] + @rolls[i+1] + @rolls [i+2]
-        i += 2 if frame == 9
-        i += 1
-      elsif spare?(i)
-        total += @rolls[i] + @rolls[i+1] + @rolls [i+2]
-        i += 2
+    while frame_index < @rolls.length-1
+      if strike?(frame_index)
+        total += 10 + @rolls[frame_index+1] + @rolls [frame_index+2]
+        frame_index += 2 if frame == 9
+        frame_index += 1
+      elsif spare?(frame_index)
+        total += 10 + @rolls [frame_index+2]
+        frame_index += 2
       else
-        total += @rolls[i] + @rolls[i+1]
-        i += 2
+        total += @rolls[frame_index] + @rolls[frame_index+1]
+        frame_index += 2
       end
       frame += 1
     end
     total
   end
 
-  def strike?(i)
-    @rolls[i] == 10
+  def strike?(frame_index)
+    @rolls[frame_index] == 10
   end
 
-  def spare?(i)
-    @rolls[i] + @rolls[i+1] == 10
+  def spare?(frame_index)
+    @rolls[frame_index] + @rolls[frame_index+1] == 10
   end
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -8,6 +8,14 @@ describe BowlingGame do
     n.times {bowling_game.roll(pins_down)}
   end
 
+  def roll_spare
+    2.times {bowling_game.roll(5)}
+  end
+
+  def roll_strike
+    bowling_game.roll(10)
+  end
+
   it "can score a gutter game" do
     roll_many(20, 0)
     expect(bowling_game.score).to eq 0
@@ -19,29 +27,27 @@ describe BowlingGame do
   end
 
   it "can score a game with a spare" do
-    bowling_game.roll(5)
-    bowling_game.roll(5)
+    roll_spare
     roll_many(18, 1)
     expect(bowling_game.score).to eq 29
   end
 
   it "can score a game with a strike" do
-    bowling_game.roll(10)
+    roll_strike
     roll_many(18, 1)
     expect(bowling_game.score).to eq 30
   end
 
   it "can score a game with a spare in the last frame" do
     roll_many(18, 1)
-    bowling_game.roll(5)
-    bowling_game.roll(5)
+    roll_spare
     bowling_game.roll(1)
     expect(bowling_game.score).to eq 29
   end
 
   it "can score a game with a strike in the last frame" do
     roll_many(18, 1)
-    bowling_game.roll(10)
+    roll_strike
     bowling_game.roll(1)
     bowling_game.roll(1)
     expect(bowling_game.score).to eq 30
